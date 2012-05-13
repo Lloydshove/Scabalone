08a9ef90c0c3e53667e03317a43fecef6e075cfa
Lloyd Shove <Email@lloydshove.com>
2012-05-13 20:07:17 +0800

    Added a Scalatest and started to thing about classes….

--- src/scabalone/Position.scala
+++ src/scabalone/Position.scala
@@ -1,5 +1,16 @@
 package scabalone
 
-class Position {
+object Colour extends Enumeration{
+  type Colour = Value
+  val Black, White = Value
+
+  implicit def ColourToOption(theColour : Colour) : Option[Colour] = {  Some(theColour)  }
+}
+
+case class Position( colour : Option[Colour.Colour] ){
+
+  def isEmpty : Boolean = {
+    !colour.isDefined
+  }
 
 }
--- test/scabalone/PositionTest.scala
+++ test/scabalone/PositionTest.scala
@@ -1,13 +1,16 @@
 package scabalone
 
-/**
- * Created with IntelliJ IDEA.
- * User: lloyd
- * Date: 13/05/2012
- * Time: 19:32
- * To change this template use File | Settings | File Templates.
- */
+import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.FunSuite
 
-class PositionTest {
+class PositionTest extends FunSuite with ShouldMatchers{
+
+  test("A Position created with no colour should be empty"){
+    new Position(None) should be ('empty)
+  }
+
+  test("A position with a colour is not empty"){
+    val blackPosition = new Position(Colour.Black)
+  }
 
 }
